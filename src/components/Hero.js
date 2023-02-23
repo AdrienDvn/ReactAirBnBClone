f6cff43b7425f3ce57bc79de7f2847b1191775c0
adriendavin <adriendavin@hotmail.fr>
2023-02-23 15:09:47 +0100

    hero section ok
    Signed-off-by: adriendavin <adriendavin@hotmail.fr>

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,10 @@ import logo2 from '/Users/adrien/code/BobZiroll/airbnbclone/src/AirBnb photo gri
 export default function Hero() {
   return (
     <hero>
-      <div className='hero'>
+      <div className='container hero--container'>
         <img src={logo2} alt="" className="hero--gridpic"/>
         <h1> Online Experiences</h1>
-        <p>blalablalabllalalalalal</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </hero>
   );
