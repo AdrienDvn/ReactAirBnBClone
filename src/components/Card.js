cb50bb9baf6461db206fa761f5ea31201d6041a1
adriendavin <adriendavin@hotmail.fr>
2023-03-01 19:41:58 +0100

    ok

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,21 +5,16 @@ export default function Card() {
   return (
     <card>
       <div className='card'>
-        <div className='card--pic container'>
-          <img src={logo3} alt="" className="hero--gridpic"/>
-          <button type="button" class="btn btn-tertiary card--button">Sold out
-          </button>
+          <img src={logo3} alt="" className="card--image"/>
+          {/* <button type="button" class="btn btn-tertiary card--button">Sold out */}
+          {/* </button> */}
+        <div className='card--stats'>
+           <span><i class="fa-solid fa-star"></i>5.0</span>
+           <span className='grey'>(6) </span>
+           <span className='grey'>USA</span>
         </div>
-        <div className='Card--infos'>
-          star icon(png)
-          <i class="fa-solid fa-star"></i>
-          rating
-          country
-          review count
-          title
-          cost/person
-        </div>
-        <p className="card--p">Card js p blablablablablablablablablablabla</p>
+          <p > Life lessons with Katie Zaferes</p>
+          <p > <span className='bold'>From 136 $USD</span> / person</p>
       </div>
     </card>
   );
