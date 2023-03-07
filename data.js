3e53c54dafc984b0a2f0bfc3e1d044361879d7ce
adriendavin <adriendavin@hotmail.fr>
2023-03-07 11:57:56 +0100

    adding a data.js

---
+++ data.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    id: 1,
+    title: "learn to swim",
+    description: "lorem ipsum blablablabla1",
+    price: 27,
+    coverImg: "img.png",
+    stats: {
+      rating: 4.7,
+      reviewCount: 53
+    },
+    location: "face to face",
+    openSpots: 27,
+  },
+  {
+    id: 2,
+    title: "learn photography",
+    description: "lorem photography ipsum blablablabla2",
+    price: 150,
+    coverImg: "img.png",
+    stats: {
+      rating: 4.4,
+      reviewCount: 37
+    },
+    location: "online",
+    openSpots: 12,
+  },
+  {
+    id: 3,
+    title: "learn piano",
+    description: "Piano lorem ipsum blablablabla3",
+    price: 25,
+    coverImg: "img.png",
+    stats: {
+      rating: 4.8,
+      reviewCount: 17
+    },
+    location: "zoom calls online",
+    openSpots: 2,
+  },
+  {
+    id: 4,
+    title: "learn to cook",
+    description: "cooking life lorem ipsum blablablabla3",
+    price: 50,
+    coverImg: "img.png",
+    stats: {
+      rating: 4.75,
+      reviewCount: 29
+    },
+    location: "face to face",
+    openSpots: 12,
+  },
+]
