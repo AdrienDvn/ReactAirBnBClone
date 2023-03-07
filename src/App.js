68634fc12324334d805734bd0c46887200d5fe84
adriendavin <adriendavin@hotmail.fr>
2023-03-07 11:32:47 +0100

    img mods

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ import Navbar from './components/Navbar.js';
 import Hero from './components/Hero.js';
 import Card from './components/Card.js';
 
-
 function App() {
   return (
     <div className="App">
       <Navbar/>
       {/* <Hero/> the exercice is to creat an instance of a card before to RE display hero :)*/}
       <Card
-        img="catPNG.png"
+        img='swimmerBnB.jpeg'
         rating="5.0"
         reviewCount= {6}
         country = "USA"
