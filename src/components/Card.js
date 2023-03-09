76bb2e306dabfab6b089dccafcab0466df8e5540
adriendavin <adriendavin@hotmail.fr>
2023-03-09 11:30:39 +0100

    conditionnal rendering badge

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,20 @@ import swimLogo from '../swimmerBnB.jpeg'
 // import image from '/Users/adrien/code/BobZiroll/airbnbclone/src/catPNG.png';
 
 export default function Card(props) {
-  // console.log(props);
+  // console.log(props.openSpots);
+  let badgeText
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.location === "online") {
+    badgeText = "Online"
+  }
 
   return (
-    <card>
+
       <div className='card'>
+        {badgeText && <div className= 'card--badge'>{badgeText}</div>}
+         {/* JS to say to display the css sold out badge only if the openSpots is empty */}
+
       {/* <img src={props.img} alt="" className="card--image"/> */}
         {/* <img src={`../${props.img}`} alt="" className="card--image"/> */}
         <img src= {swimLogo} alt="" className="card--image"/>
@@ -20,6 +29,6 @@ export default function Card(props) {
         <p > {props.description}</p>
         <p > <span className='bold'>From {props.price} $USD</span> / person</p>
       </div>
-    </card>
+
   );
 }
